7c4721cbeafb8010a413981517066cb572770a14
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2021-04-29 02:10:47 +0200

    Update MarsStation.h

--- Main/MarsStation/MarsStation.h
+++ Main/MarsStation/MarsStation.h
@@ -1,10 +1,5 @@
-#include "..\Missions\EmergencyMission.h"
-#include "..\Missions\MountainousMission.h"
-#include "..\Missions\PolarMission.h"
-#include "..\Rovers\EmergencyRover.h"
-#include "..\Rovers\MountainousRover.h"
-#include "..\Rovers\PolarRover.h"
 #pragma once
+#include "..\All_Files.h"
 class MarsStation
 {
 
