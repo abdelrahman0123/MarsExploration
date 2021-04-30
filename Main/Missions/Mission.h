21bbd50f92a66ce30de2bb09563e21ac1f2e72ce
doaa281 <65799105+doaa281@users.noreply.github.com>
2021-05-01 01:43:17 +0200

    Update Mission.h

--- Main/Missions/Mission.h
+++ Main/Missions/Mission.h
@@ -13,43 +13,43 @@ public:
 		WD = 0; ED = 0; CD = 0;
 	}
 	//getters
-	int getid(){ return ID; }
-	int gettarloc(){ return TLOC; }
-	int getmissdur(){ return MDUR; }
-	int getsig(){ return SIG; }
-	int getformday(){ return FD; }
-	int getwaitdays(){ return WD; }
-	int getexecdays(){ return ED; }
-	int getcompleteday(){ return CD; }
+	int GetId(){ return ID; }
+	int GetTarLoc(){ return TLOC; }
+	int GetMissDur(){ return MDUR; }
+	int GetSig(){ return SIG; }
+	int GetFormDay(){ return FD; }
+	int GetWaitDays(){ return WD; }
+	int GetExecDays(){ return ED; }
+	int GetCompleteDay(){ return CD; }
 
 
-	int getavgwaiting(int num)
+	int GetAvgWaiting(int num)
 	{
 		return WD / num;
 	}
-	int getavgexec(int num)
+	int GetAvgExec(int num)
 	{
 		return ED / num;
 	}
 	//setters
-	void setid(int i){ ID = (i> 0) ? i : 0; }
-	void settarloc(int l){ TLOC = (l > 0) ? l : 0; }
-	void setmissdur(int i){ MDUR = (i > 0) ? i : 0; }
-	void setsig(int i){ SIG = (i > 0) ? i : 0; }
-	void setformday(int i){ FD = (i > 0) ? i : 0; }
-	void incrwaitingdays(){ WD++; }//waiting each mission
+	void SetId(int i){ ID = (i> 0) ? i : 0; }
+	void SetTarLoc(int l){ TLOC = (l > 0) ? l : 0; }
+	void SetMissDur(int i){ MDUR = (i > 0) ? i : 0; }
+	void SetSig(int i){ SIG = (i > 0) ? i : 0; }
+	void SetFormDay(int i){ FD = (i > 0) ? i : 0; }
+	void IncrWaitingDays(){ WD++; }//waiting each mission
 
-	void setexecdays(int speed)
+	void SetExecDays(int speed)
 	{
 		int way = (TLOC / speed) / 25;
 		ED = 2 * way + MDUR;
 	}
-	void setcompleteday()
+	void SetCompleteDay()
 	{
 		CD = FD + WD + ED;
 	}
 	//print format
-	void printmission()
+	void PrintMission()
 	{
 		cout << CD << "\t" << ID << "\t" << FD << "\t" << WD << "\t" << ED << endl;
 	}
