7b02425f8b2eb4d55f31bea37a4490401cdc7c08
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2021-05-01 06:42:59 +0200

    MarsStation Updates

--- Main/MarsStation/MarsStation.h
+++ Main/MarsStation/MarsStation.h
@@ -3,18 +3,49 @@
 class MarsStation
 {
 	// Lists
-	Queue<Event> Events;
-	PriQ<EmergencyMission> EmergencyMissions;
-	Queue<Event> MountainousMissions;
-	Queue<Event> PolarMissions;
-	PriQ<Event> EmergencyRovers;
-	PriQ<Event> MountainousRovers;
-	PriQ<Event> PolarRovers;
-	PriQ<Event> InExecutionMissions;
-	PriQ<Event> InExecutionRovers;
-	PriQ<Event> MaintenanceRovers;
-	Queue<Mission> CompletedMissions; // 3adel el types
+	Queue<Event*> Events;
+	PriQ<EmergencyMission*> EmergencyMissions;
+	Queue<MountainousMission*> MountainousMissions;
+	Queue<PolarMission*> PolarMissions;
+	PriQ<EmergencyRover*> EmergencyRovers;
+	PriQ<MountainousRover*> MountainousRovers;
+	PriQ<PolarRover*> PolarRovers;
+	PriQ<Mission*> InExecutionMissions;
+	PriQ<Rover*> InExecutionRovers;
+	PriQ<Rover*> MaintenanceRovers;
+	Queue<Mission*> CompletedMissions;
 public:
+	// Default Constructor
 	MarsStation();
+
+	// Add to Lists
+	void AddToEmergencyMissions(EmergencyMission* EM, int sig);
+	void AddToMountainousMissions(MountainousMission* MM);
+	void AddToPolarMissions(PolarMission* PM);
+	void AddToEmergencyRovers(EmergencyRover* ER, int speed);
+	void AddToMountainousRovers(MountainousRover* MR, int speed);
+	void AddToPolarRovers(PolarRover* PR, int speed);
+	void AddToInExecutionMissions(Mission* M, int n);
+	void AddToInExecutionRovers(Rover* R, int n);
+	void AddToMaintenanceRovers(Rover* R, int n);
+	void AddToCompletedMissions(Mission* EM);
+
+	// Remove from Lists
+	EmergencyMission* RemoveFromEmergencyMissions();
+	MountainousMission* RemoveFromMountainousMissions();
+	PolarMission* RemoveFromPolarMissions();
+	EmergencyRover* RemoveFromEmergencyRovers();
+	MountainousRover* RemoveFromMountainousRovers();
+	PolarRover* RemoveFromPolarRovers();
+	Mission* RemoveFromInExecutionMissions();
+	Rover* RemoveFromInExecutionRovers();
+	Rover* RemoveFromMaintenanceRovers();
+	Mission* RemoveFromCompletedMissions();
+
+	// Cancel Mountainous Mission
+	void CancelMountainous(MountainousMission* MM);
+
+	// Promote Mountainous Mission
+	void PromoteMountainous(MountainousMission* MM);
 };
 
