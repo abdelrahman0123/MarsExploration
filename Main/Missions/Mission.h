aa7e69d5077986b070b38c7e19dbf643c8ff32d4
doaa281 <65799105+doaa281@users.noreply.github.com>
2021-05-03 06:02:02 +0200

    m

--- Main/Missions/Mission.h
+++ Main/Missions/Mission.h
@@ -7,50 +7,35 @@ private:
 	int ID, TLOC, MDUR, SIG, FD, WD, ED, CD;
 	// id  location, duration,  significance, formulation day,waiting days,execution days,completion days
 public:
-	Mission() :WD(0), ED(0), CD(0){}
-	Mission(int a, int b, int c, int d, int e) :ID(a), TLOC(b), MDUR(c), SIG(d), FD(e)
-	{
-		WD = 0; ED = 0; CD = 0;
-	}
+	//constuctor
+	Mission();
+	Mission(int a, int b, int c, int d, int e);
+	//destuctor
+	~Mission();
+	
 	//getters
-	int GetId(){ return ID; }
-	int GetTarLoc(){ return TLOC; }
-	int GetMissDur(){ return MDUR; }
-	int GetSig(){ return SIG; }
-	int GetFormDay(){ return FD; }
-	int GetWaitDays(){ return WD; }
-	int GetExecDays(){ return ED; }
-	int GetCompleteDay(){ return CD; }
+	int GetId();
+	int GetTargetLocation();
+	int GetMissDuration();
+	int GetSignificance();
+	int GetFormulationDay();
+	int GetWaitingDays();
+	int GetExecutionDays();
+	int GetCompletionDay();
+	int GetAvgWaitingdays(int num);
+	int GetAvgExecutiondays(int num);
 
-
-	int GetAvgWaiting(int num)
-	{
-		return WD / num;
-	}
-	int GetAvgExec(int num)
-	{
-		return ED / num;
-	}
 	//setters
-	void SetId(int i){ ID = (i> 0) ? i : 0; }
-	void SetTarLoc(int l){ TLOC = (l > 0) ? l : 0; }
-	void SetMissDur(int i){ MDUR = (i > 0) ? i : 0; }
-	void SetSig(int i){ SIG = (i > 0) ? i : 0; }
-	void SetFormDay(int i){ FD = (i > 0) ? i : 0; }
-	void IncrWaitingDays(){ WD++; }//waiting each mission
+	void SetId(int i);
+	void SetTargetLocation(int l);
+	void SetMissionDuration(int i);
+	void SetSignificance(int i);
+	void SetFormulationDay(int i);
+	void IncrementWaitingDays();
+    void SetExecutionDays(int speed);
+	void SetCompletionDay();
 
-	void SetExecDays(int speed)
-	{
-		int way = (TLOC / speed) / 25;
-		ED = 2 * way + MDUR;
-	}
-	void SetCompleteDay()
-	{
-		CD = FD + WD + ED;
-	}
+
 	//print format
-	void PrintMission()
-	{
-		cout << CD << "\t" << ID << "\t" << FD << "\t" << WD << "\t" << ED << endl;
-	}
+	void PrintMission();
 };
