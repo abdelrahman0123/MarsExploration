b74ec7f33958c5b56c849f1dc298364a834672bb
SarahElzayat <sarah.sayed00@eng-st.cu.edu.eg>
2021-05-27 20:25:07 +0200

    000

--- Main/UI/UI.cpp
+++ Main/UI/UI.cpp
@@ -7,39 +7,25 @@ UI::UI()
 	polarRovers = new Queue<int>;
 	mountRovers = new Queue<int>;
 }
-
-void UI::readRovers()
-{
-	if (ipFile.is_open())
-	{
-		int x, y, z, temp;
-		//Getting rovers no.
-		ipFile >> x >> y >> z;
-		//Getting max no. of rovers
-		(x > y) ? temp = x : temp = y;
-		(temp > z) ? temp = temp : temp = z;
-		//Getting rovers' speeds
-		for (int i = 0; i < temp; i++)
-			for (int i = 0; i < 3; i++)
-			{
-				ipFile >> x;
-				if (x != -1)
-				{
-					switch (i)
-					{
-					case 0:
-						mountRovers->enqueue(x);
-						break;
-					case 1:
-						polarRovers->enqueue(y);
-						break;
-					case 2:
-						emergencyRovers->enqueue(z);
-						break;
-					}
-				}
-			}
-	}
+/*UI::UI(int p, int em, int m) {
+	setPolarRoversNum(p);
+	setEmergencyRoversNum(em);
+	setMountainousRoversNum(m);
+	polarList = new int[polarRoversNum];
+	emergencyList = new int[emergencyRoversNum];
+	mountainousList = new int[mountainousRoversNum];
+}
+void UI::setEmergencyRoversNum(int s) {
+	emergencyRoversNum = s;
+}
+void UI::setPolarRoversNum(int s) {
+	polarRoversNum = s;
+}
+void UI::setMountainousRoversNum(int s) {
+	mountainousRoversNum = s;
+}
+int UI::getEmergencyRoversNum() {
+	return emergencyRoversNum;
 }
 
 int UI::getAutoP()
@@ -97,62 +83,128 @@ Queue<int>* UI::getEmRovers()
 	return emergencyRovers;
 }
 
-Queue<int>* UI::getPolarRovers()
-{
-	return polarRovers;
+	cout << "\n-------------------------------------------------------------------------\n";
+}*/
+void UI:: addAvPolar(int n){
+	avPolar.enqueue(n);
+	avPolarRoversnum++;
 }
 
-Queue<int>* UI::getMountRovers()
-{
-	return mountRovers;
+void UI:: addAvEmergency(int n){
+	avEmergency.enqueue(n);
+	avEmergencyRoversnum++;
+}
+void UI:: addAvMountainous(int n){
+	avMountainous.enqueue(n);
+	avMountainounsRoversnum++;
+}
+void UI:: addCheckupPolar(int n){
+	checkupPolar.enqueue(n);
+	checkupPolarRoversnum++;
+
+}
+void UI:: addCheckupEmemergnecy(int n){
+	checkupEmemergnecy.enqueue(n);
+	checkupEmergencyRoversnum++;
+}
+void UI:: addCheckupMountainous(int n){
+checkupMountainous.enqueue(n);
+checkupMountainousRoversnum++;
+}
+void UI:: addWaitingMissionsPolar(int n){
+	waitingMissionsPolar.enqueue(n);
+	waitingPolarMissionsnum++;
+}
+void UI:: addWaitingMissionsEmergency(int n){
+waitingMissionsEmergency.enqueue(n);
+waitingEmergemcyMissionsnum++;
+}
+void UI:: addWaitingMissionsMountainous(int n){
+waitingMissionsMountainous.enqueue(n);
+waitingMountainousMissionsnum++;
+}
+void UI:: addInExecutionPolar(int n){
+inExecutionPolar.enqueue(n);
+inExecutionPolarMissionsnum++;
+}
+void UI:: addInExecutionEmergency(int n){
+inExecutionEmergency.enqueue(n);
+inExecutiomEmergencyMissionsnum++;
+}
+void UI:: addInExecutionMountainous(int n){
+inExecutionMountainous.enqueue(n);
+inExecutionMountainousMissionsnum++;
+
+}
+void UI:: addCompletedPolarMissions(int n){
+completedPolarMissions.enqueue(n);
+compPolarMissionsnum++;
+}
+void UI:: addCompletedEmergencyMisisons(int n){
+completedEmergencyMisisons.enqueue(n);
+compEmergencyMissionsnum++;
+}
+void UI:: addCompletedMountainousMissions(int n){
+completedMountainousMissions.enqueue(n);
+comMountainousMissionsnum++;
 }
 
-void UI::setEtype(char E)
-{
-	switch (E)
-	{
-	case 'F':
-		ET = Formulation;
-		break;
-	case 'X':
-		ET = Cancel;
-		break;
-	case 'P':
-		ET = Promote;
-		break;
-	}
-}
 
-EventType UI::getET()
-{
-	return ET;
+void UI::removeAvEmergency(  ) { int n;
+	avEmergency.dequeue(n);
+	avEmergencyRoversnum--;
 }
+void UI::removeAvMountainous(  ) { int n;
+	avMountainous.dequeue(n);
+	avMountainounsRoversnum--;
+}
+void UI::removeCheckupPolar(  ) { int n;
+	checkupPolar.dequeue(n);
+	checkupPolarRoversnum--;
 
-void UI::setMtype(char M)
-{
-	switch (M)
-	{
-	case 'E':
-		MT = Emergency;
-		break;
-	case 'M':
-		MT = Mountainous;
-		break;
-	case 'P':
-		MT = Polar;
-		break;
-		
-	}
 }
+void UI::removeCheckupEmemergnecy(  ) { int n;
+	checkupEmemergnecy.dequeue(n);
+	checkupEmergencyRoversnum--;
+}
+void UI::removeCheckupMountainous(  ) { int n;
+	checkupMountainous.dequeue(n);
+	checkupMountainousRoversnum--;
+}
+void UI::removeWaitingMissionsPolar(  ) { int n;
+	waitingMissionsPolar.dequeue(n);
+	waitingPolarMissionsnum--;
+}
+void UI::removeWaitingMissionsEmergency(  ) { int n;
+	waitingMissionsEmergency.dequeue(n);
+	waitingEmergemcyMissionsnum--;
+}
+void UI::removeWaitingMissionsMountainous(  ) { int n;
+	waitingMissionsMountainous.dequeue(n);
+	waitingMountainousMissionsnum--;
+}
+void UI::removeInExecutionPolar(  ) { int n;
+	inExecutionPolar.dequeue(n);
+	inExecutionPolarMissionsnum--;
+}
+void UI::removeInExecutionEmergency(  ) { int n;
+	inExecutionEmergency.dequeue(n);
+	inExecutiomEmergencyMissionsnum--;
+}
+void UI::removeInExecutionMountainous(  ) { int n;
+	inExecutionMountainous.dequeue(n);
+	inExecutionMountainousMissionsnum--;
 
-MissionType UI::getMT()
-{
-	return MT;
 }
-
-/*
-Queue<int>* UI::getEventData()
-{
-	return eventData;
+void UI::removeCompletedPolarMissions(  ) { int n;
+	completedPolarMissions.dequeue(n);
+	compPolarMissionsnum--;
 }
-*/
+void UI::removeCompletedEmergencyMisisons(  ) { int n;
+	completedEmergencyMisisons.dequeue(n);
+	compEmergencyMissionsnum--;
+}
+void UI::removeCompletedMountainousMissions(  ) { int n;
+	completedMountainousMissions.dequeue(n);
+	comMountainousMissionsnum--;
+}
--- Main/UI/UI.h
+++ Main/UI/UI.h
@@ -5,8 +5,9 @@
 #include "../EventType.h"
 using namespace std;
 #pragma once
-class UI
-{
+#include"../DataStructures/Queue.h"
+class UI{
+/*
 private:
 	ifstream ipFile;
 	ofstream opFile;
@@ -39,6 +40,72 @@ public:
 	MissionType getMT();
 	//Event data getter
 	//Queue<int>* getEventData();
+	*/
+	int avPolarRoversnum =0, avEmergencyRoversnum  = 0, avMountainounsRoversnum  = 0,
+		checkupPolarRoversnum  = 0, checkupEmergencyRoversnum =0, checkupMountainousRoversnum =0,
+		waitingPolarMissionsnum =0, waitingEmergemcyMissionsnum =0, waitingMountainousMissionsnum =0,
+		inExecutionPolarMissionsnum =0, inExecutionMountainousMissionsnum =0, inExecutiomEmergencyMissionsnum =0,
+		compPolarMissionsnum =0, compEmergencyMissionsnum =0, comMountainousMissionsnum =0
+		, polarCheckupnum =0, emergencyCheckupnum =0, mountainousCheckupnum =0;
+
+	Queue<int> avPolar, avEmergency, avMountainous;
+	Queue<int> checkupPolar, checkupEmemergnecy, checkupMountainous, waitingMissionsPolar, waitingMissionsEmergency;
+	Queue<int> waitingMissionsMountainous, inExecutionPolar, inExecutionEmergency, inExecutionMountainous;
+	Queue<int> completedPolarMissions, completedEmergencyMisisons, completedMountainousMissions;
+
+public:
+	UI();
+	UI(int p, int em, int m);
+	/*void setPolarRoversNum(int n);
+	void setEmergencyRoversNum(int n);
+	void setMountainousRoversNum(int n);
+	void setPolarList(int*pList);
+	void setEmergencyList(int* sList);
+	void setMountainousList(int* mList);
+	void setPolarCheckupList(int* pList);
+	void setEmergencyCheckupList(int* emList);
+	void setMountainousCheckupList(int* mList);
+	int getPolarRoversNum();
+	int getEmergencyRoversNum();
+	int getMountainousRoversNum();
+	//printing
+	void print();
+	void printWaintingMissions();
+	void printInExecutionMissions();
+	void printAvailabeRovers();
+	void printInCheckupRovers();
+	void printCompletedMissions();*/
+	void addAvPolar(int n);
+	void addAvEmergency(int n);
+	void addAvMountainous(int n);
+	void addCheckupPolar(int n);
+	void addCheckupEmemergnecy(int n);
+	void addCheckupMountainous(int n);
+	void addWaitingMissionsPolar(int n);
+	void addWaitingMissionsEmergency(int n);
+	void addWaitingMissionsMountainous(int n);
+	void addInExecutionPolar(int n);
+	void addInExecutionEmergency(int n);
+	void addInExecutionMountainous(int n);
+	void addCompletedPolarMissions(int n);
+	void addCompletedEmergencyMisisons(int n);
+	void addCompletedMountainousMissions(int n);
+	void removeAvPolar(  );
+	void removeAvEmergency(  );
+	void removeAvMountainous(  );
+	void removeCheckupPolar(  );
+	void removeCheckupEmemergnecy(  );
+	void removeCheckupMountainous(  );
+	void removeWaitingMissionsPolar(  );
+	void removeWaitingMissionsEmergency(  );
+	void removeWaitingMissionsMountainous(  );
+	void removeInExecutionPolar(  );
+	void removeInExecutionEmergency(  );
+	void removeInExecutionMountainous(  );
+	void removeCompletedPolarMissions(  );
+	void removeCompletedEmergencyMisisons(  );
+	void removeCompletedMountainousMissions(  );
+
 
 };
 
